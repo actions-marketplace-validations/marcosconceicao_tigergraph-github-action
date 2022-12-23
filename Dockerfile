8565a76ccbaf8e0075e9638d9c5f36689f990a16
Marcos Conceicao <marcos.conceicao@nubank.com.br>
2022-12-23 17:23:02 -0300

    update command

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,4 @@
-# Container image that runs your code
-FROM alpine:3.10
-
-# Copies your code file from your action repository to the filesystem path `/` of the container
-COPY entrypoint.sh /entrypoint.sh
-
-# Code file to execute when the docker container starts up (`entrypoint.sh`)
-ENTRYPOINT ["/entrypoint.sh"]
+FROM docker:stable
+COPY start-tgserver.sh /start-tgserver.sh
+RUN chmod +x /start-tgserver.sh
+ENTRYPOINT ["/start-tgserver.sh"]
---
+++ start-tgserver.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Map input values from the GitHub Actions workflow to shell variables
+TGSERVER_VERSION="3.7.0"
+TGSERVER_USERNAME="tigergraph"
+TGSERVER_PASSWORD="tigergraph"
+
+
+if [ -z "$TGSERVER_VERSION" ]; then
+  echo ""
+  echo "Missing TigerGraph version in the [tgserver-version] input. Received value: $TGSERVER_VERSION"
+  echo ""
+
+  exit 2
+fi
+
+
+echo "::group::Starting TigerGraph Server"
+echo "  - version [$TGSERVER_VERSION]"
+echo ""
+
+docker run --name tigergraph -d -p 14022:22 -p 9000:9000 -p 14240:14240 --ulimit nofile=1000000:1000000 -v ~/data:/home/tigergraph/mydata -t tigergraph/tigergraph:$TGSERVER_VERSION
+
+if [ $? -ne 0 ]; then
+    echo "Error starting TGSERVER Docker container"
+    exit 2
+fi
+echo "::endgroup::"
+
+
+echo "::group:: Waiting for TGSERVER to accept connections"
+sleep 1
+TIMER=0
+
+until docker exec -it $(docker ps | grep tigergraph | awk '{print $1}') /bin/sh # &> /dev/null
+do
+  sleep 1
+  echo "."
+  TIMER=$((TIMER + 1))
+
+  if [[ $TIMER -eq 20 ]]; then
+    echo "TGSERVER did not initialize within 20 seconds. Exiting."
+    exit 2
+  fi
+done
+echo "::endgroup::"
